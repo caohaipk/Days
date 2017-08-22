5ac23052d724ac29d42ff1fede1f1e07690cb686
Gray <mrhaidff@gmail.com>
2017-08-22 16:53:59 +0700

    Thêm Bottom nav bar

--- app/src/main/java/com/wordpress/grayfaces/days/Activity/MainActivity.java
+++ app/src/main/java/com/wordpress/grayfaces/days/Activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.wordpress.grayfaces.days.Activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
@@ -13,6 +15,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.wordpress.grayfaces.days.Fragment.HomeFragment;
 import com.wordpress.grayfaces.days.R;
@@ -27,15 +30,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -44,6 +38,28 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_camera:
+                        Toast.makeText(MainActivity.this, "Camera", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_gallery:
+                        Toast.makeText(MainActivity.this, "Gallery", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_slideshow:
+                        Toast.makeText(MainActivity.this, "Slidershow", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_manage:
+                        Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
         Render();
     }
 
